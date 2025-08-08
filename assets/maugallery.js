e8815a25f03c506addadc9e592d4162afb8696df
Carmedine <carmedine@protonmail.com>
2025-08-08 05:40:43 +0200

    redimension et compression des photos

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -151,10 +151,11 @@
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
           index = i ;
+
         }
       });
       next =
-        imagesCollection[index] ||
+        imagesCollection[index-1] ||//ajout du -1 
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -192,7 +193,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index+1] || imagesCollection[0];//ajout du +1
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
@@ -240,7 +241,7 @@
         return;
       }
       $(".active-tag").removeClass("active active-tag");
-      $(this).addClass("active-tag");
+      $(this).addClass(" active active-tag"); // ajout class active pour surbrillance du filtre
 
       var tag = $(this).data("images-toggle");
 
